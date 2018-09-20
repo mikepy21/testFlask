af0f25215a962ac7e25525a7506cbfda08aebd5d
Mike Peralta <mike.macys.sf@gmail.com>
2018-09-20 10:40:23 -0700

    new update1

---
+++ flask1.py
@@ -0,0 +1,14 @@
+from flask import jsonify
+from flask import Flask
+app = Flask(__name__)
+
+l=[]
+l.append(1)
+l.append('a')
+l.append("jsdkjskj")
+
+@app.route("/")
+def hello_world():
+	resp = jsonify(success=False)
+	resp = jsonify(objd=l,success='success',code=200)
+	return resp
---
+++ flask2.py
@@ -0,0 +1,8 @@
+# flask depends on this env variable to find the main file
+export FLASK_APP=hello.py
+
+# now we just need to ask flask to run
+flask run
+
+# * Serving Flask app "hello"
+# * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
